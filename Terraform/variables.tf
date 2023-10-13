5d5865f7bb2353494e01d46c77f8a70e1da96858
John Kennedy <sodavim763@orlydns.com>
2023-10-13 16:39:23 -0400

    Updated terraform files

---
+++ Terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "AWS region"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key to be used for SSH access"
+}
+
+variable "private_key_path" {
+  description = "Path to the private key to be used for SSH connections"
+}
+
+variable "key_name" {
+  description = "Key pair name"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+}
+
+variable "instance_count" {
+  description = "Number of instances to launch"
+}
+
+variable "username" {
+  description = "Username for SSH connections"
+}
---
+++ Terraform/variables.tfvars
@@ -0,0 +1,7 @@
+region          = "us-east-1"
+public_key_path = "/home/cloudshell-user/mykey.pub"
+private_key_path = "/home/cloudshell-user/mykey"
+key_name        = "mykey"
+instance_type   = "t2.micro"
+instance_count  = 3
+username        = "admin"
